0846d6c158b148e1003dece884cf806fb0e9fa5b
thinhnnd <16521179@gm.uit.edu.vn>
2019-11-04 08:49:35 +0700

    add splash screen and some icon

--- App.js
+++ App.js
@@ -33,6 +33,7 @@ async function loadResourcesAsync() {
     Asset.loadAsync([
       require('./assets/images/robot-dev.png'),
       require('./assets/images/robot-prod.png'),
+      require('./assets/images/uit-ctf-time.png'),
     ]),
     Font.loadAsync({
       // This is the font that we are using for our tab bar
