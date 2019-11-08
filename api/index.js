66cdb8df3330868be70c56cfabe6f13f44ebc863
thinhnnd <16521179@gm.uit.edu.vn>
2019-11-08 16:22:54 +0700

    Team screen add api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import * as ctfEventApi from './ctf-event.api';
 import * as auth from './auth.api';
+import * as teamsApi from './teams.api';
 
 export default API_HELPERS = {
     ...ctfEventApi,
-    ...auth
+    ...auth,
+    ...teamsApi
 }
